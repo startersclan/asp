b838e931d795200557258c9728ab4b3c5eb75626
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2022-10-01 00:31:48 +0000

    Docs: Add a full BF2 stack example in `docs/full-bf2-stack-example`

---
+++ docs/full-bf2-stack-example/config/ASP/config.php
@@ -0,0 +1,54 @@
+<?php
+/***************************************
+*  Battlefield 2 Private Stats Config  *
+****************************************
+* All comments have been removed from  *
+* this file. Please use the Web Admin  *
+* to change values.                    *
+***************************************/
+$admin_user = 'admin';
+$admin_pass = 'admin';
+$admin_hosts = array('0.0.0.0/0','192.1.0.1/32');
+$admin_ignore_ai = 1;
+$admin_last_login = 1619049094;
+$admin_current_login = 1619062147;
+$admin_timezone = 'America/Los_Angeles';
+$debug_lvl = 4;
+$db_host = 'db';
+$db_port = 3306;
+$db_user = 'root';
+$db_pass = 'ascent';
+$db_name = 'bf2stats';
+$stats_strict_api = 0;
+$stats_aspx_cache_time = 30;
+$stats_risingstar_refresh = 1619049068;
+$stats_smoc_refresh = 1618270660;
+$stats_general_refresh = 1617845422;
+$stats_ignore_ai = 0;
+$stats_min_game_time = 0;
+$stats_min_player_game_time = 0;
+$stats_players_min = 1;
+$stats_players_max = 256;
+$stats_awds_complete = 0;
+$stats_rank_complete = 0;
+$stats_lan_override = '174.49.21.221';
+$stats_save_snapshot = 1;
+$stats_risingstar_interval = 7;
+$stats_smoc_interval = 7;
+$stats_smoc_last_select = 1618270691;
+$stats_general_interval = 31;
+$stats_general_last_select = 1618270681;
+$stats_general_mode = 0;
+$game_unlocks = 0;
+$game_unlocks_bonus = 2;
+$game_unlocks_bonus_min = 1;
+$game_awds_ignore_time = 0;
+$battlespy_enable = 1;
+$battlespy_max_spm = 7;
+$battlespy_max_kpm = 10;
+$battlespy_max_target_kills = 10;
+$battlespy_max_team_kills = 5;
+$battlespy_max_awards = 0;
+$battlespy_max_accuracy = 50;
+$battlespy_weapons = array('0','2','3','4','5','7','8');
+$battlespy_rank_check = 1;
