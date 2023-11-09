1301bbe7d4bf4645db82a1d594a1003f5d6e10a5
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2023-11-09 17:40:22 +0000

    Tail logs with --retry

--- config/ASP/tail.sh
+++ config/ASP/tail.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -eu
-while true; do
-    echo "Tailing logs in /src/ASP/system/logs/*.log"
-    tail -n0 -f /src/ASP/system/logs/*.log || sleep 10;
-done
+echo "Tailing logs in /src/ASP/system/logs/*.log"
+tail -n0 -F \
+    /src/ASP/system/logs/asp_debug.log \
+    /src/ASP/system/logs/php_errors.log \
+    /src/ASP/system/logs/stats_debug.log \
+    2>/dev/null
